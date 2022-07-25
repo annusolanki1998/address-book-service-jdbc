cb00d1e08ca71ffffdd10e7566437b1819f45cf5
Annu <annusinghraj1998@gmail.com>
2022-07-25 19:54:16 +0530

    ADDED: Connected database to java program and retrieved all the data from database table

--- src/main/java/com/bridgelabz/AddressBookService.java
+++ src/main/java/com/bridgelabz/AddressBookService.java
@@ -1,9 +1,33 @@
 package com.bridgelabz;
 
+import java.sql.*;
+
 public class AddressBookService {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to address book service");
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/address_book_service";
+        String userName = "root";
+        String password = "12345";
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded");
+            connection = DriverManager.getConnection(jdbcUrl, userName, password);
+            System.out.println("Connection done...");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from address_book");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5) + " " + resultSet.getString(6) + " " + resultSet.getString(7) + " " + resultSet.getString(8) + " " + resultSet.getString(9) + " " + resultSet.getString(10));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        } finally {
+            connection.close();
+        }
     }
 
 }
