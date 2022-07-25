ba12584659c6f71acd7c3f6da8e5ad236b23ab61
Annu <annusinghraj1998@gmail.com>
2022-07-25 19:29:52 +0530

    ADDED: Welcome to address book service program

---
+++ src/main/java/com/bridgelabz/AddressBookService.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class AddressBookService {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book service");
+    }
+
+}
